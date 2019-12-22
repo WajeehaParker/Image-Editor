3e13aa5224dcab75bd676c260ed890ddfade6930
WajeehaParker <wajeeha.parker@gmail.com>
2019-12-22 15:03:45 +0500

    blur effect added

--- script.js
+++ script.js
@@ -26,6 +26,39 @@ function invert() {
     context.putImageData(imageData, 0, 0);
 }
 
+function blurfunc() {
+    canvas = document.getElementById('can');
+    context = canvas.getContext('2d');
+    var imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+    var data = imageData.data;
+    
+    for (var br=0; br<10; br+=1) {
+        for (var i=0, n=data.length; i<n; i+=4) {
+            var imgW = 4*canvas.width;
+            var op = r = g = b = 0;
+            var count = 0;
+            // data of surrounding pixels
+            var CloseData = [i-imgW-4, i-imgW, i-imgW+4, i+imgW-4, i+imgW, i+imgW+ 4];
+            // calculating Sum value of all close pixels
+            for (var e=0; e<CloseData.length; e+=1){
+                if (CloseData[e] >= 0 && CloseData[e] <= data.length-3) {
+                    op += data[CloseData[e]];
+                    r += data[CloseData[e] + 1];
+                    g += data[CloseData[e] + 2];
+                    b += data[CloseData[e] + 3];
+                    count += 1;
+                }
+            }
+            // applying average values
+            data[i] = (op / count)*1;
+            data[i+1] = (r / count)*1;
+            data[i+2] = (g / count)*1;
+            data[i+3] = (b / count)+1;
+        }
+    }
+    context.putImageData(imageData, 0, 0);
+}
+
 //function black_white() {
 //    var image=document.getElementById('image');
 //  //change all pixels of image to gray
